d114b3e26f6be2122e8a6ba28378fd900aa5e9fa
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-04-06 11:46:28 +0200

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/contact'
+  get 'pages/about'
+  get 'pages/portefolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'portefolio', to: 'pages#portefolio', as: :portefolio
 end
