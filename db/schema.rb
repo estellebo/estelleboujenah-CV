3575f00be63ecf10fce881c47e154b57088a9c07
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-04-11 18:17:53 +0200

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,12 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_08_163005) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
